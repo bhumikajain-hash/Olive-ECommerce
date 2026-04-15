199ff97a3ba74b0c637c1990a4d34ca916d01168
Bhumika Jain <jainbhumika666@gmail.com>
2026-04-15 21:34:12 +0530

    ECommerce backend update

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 // We create a custom instance of Axios with our default settings
 const axiosConfig = axios.create({
   // 1. The Base URL: Now you never have to type this again!
-  baseURL: 'http://localhost:3000',
+ // baseURL: 'http://localhost:3000',
+ // @ts-ignore
+ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
   
 });
 
